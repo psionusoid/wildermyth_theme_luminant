875a6997575cde05072598524f118f75ff03aece
psionusoid <psionusoid@gmail.com>
2023-06-03 12:27:04 -0600

    added more options for creating tintable layers

--- assets/figures/images/human/misc/layer_colorshift.R
+++ assets/figures/images/human/misc/layer_colorshift.R
@@ -15,15 +15,22 @@ files_no_ext <- tools::file_path_sans_ext(files)
 n_files <- length(files)
 
 #function to convert to grayscale (requires alpha value)
-rgb2gray <- function(rgb){
-  weights <- c(.299,.587,.114)
-  gray_val <- sum(rgb[1:3]*weights)
+rgb2gray <- function(rgb, method = "weighted"){
+  if(method == "weighted"){
+    weights <- c(.299,.587,.114)
+    gray_val <- sum(rgb[1:3]*weights)
+  } else if(method == "max"){
+    gray_val <- max(rgb[1:3])
+  } else if(method == "white"){
+    gray_val <- 1
+  }
+  
   gray <- c(rep(gray_val,3),rgb[4])
 }
 
 for(i in seq_along(files)){
   image <- readPNG(files[i])
-  image_gray <- apply(image, MARGIN = c(1,2), FUN = rgb2gray)
+  image_gray <- apply(image, MARGIN = c(1,2), FUN = rgb2gray, method = "white")
   #transpose array to be same as original
   image_gray <- aperm(image_gray, perm = c(2,3,1))
   #write out grayscale png
