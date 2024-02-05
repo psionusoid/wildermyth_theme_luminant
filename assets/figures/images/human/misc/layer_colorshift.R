126f89c8dcc0919bf6b5e8d0c1cd4f740e2c2e08
psionusoid <psionusoid@gmail.com>
2024-02-04 23:34:30 -0700

    started updates to script for color adjustments

--- assets/figures/images/human/misc/layer_colorshift.R
+++ assets/figures/images/human/misc/layer_colorshift.R
@@ -17,7 +17,19 @@ solid_color <- c("#FF2020", #red
                  "#00FFFF", #cyan
                  "#6060FF", #blue
                  "#FF40FF", #magenta
-                 "#202020") #shadowy
+                 "#202020", #shadowy
+                 "#FFFFFF") #white
+#how much to adjust the opacity for each color
+alpha_scale_base <- 0.8 #base
+alpha_scale_tint <- 0.8 #tint-able
+alpha_scale <- c(0.8, #red
+                 0.8, #orange
+                 0.8, #green
+                 0.8, #cyan
+                 0.8, #blue
+                 0.8, #magenta
+                 0.8, #shadowy
+                 0.8) #white
 
 #what to add to the filename for these sets of images
 file_name_modifier <- sapply(solid_color, FUN = function(x) paste0("_", paste0(strsplit(x, split = "")[[1]][-1], collapse = "")))
@@ -29,6 +41,10 @@ files <- all_files[c(grep("L.png", all_files),
                      grep("R.png", all_files),
                      grep("tF.png", all_files),
                      grep("M.png", all_files))]
+#find files of radiant layers for alpha scaling
+files_radiant <- files[c(grep("R.png", all_files),
+                             grep("ntF.png", all_files),
+                             grep("ntM.png", all_files))]
 
 #strip extensions
 files_no_ext <- tools::file_path_sans_ext(files)
@@ -61,6 +77,46 @@ rgb2solid <- function(image, color){
   return(image)
 }
 
+#function to scale alpha values
+scale_alpha <- function(image, alpha_scale){
+  image[,,4] <- alpha_scale*image[,,4]
+  #drop alpha to 1 if greater than 1
+  image[,,4][image[,,4] >1] <- 1
+}
+
+#base color
+for(i in seq_along(files)){
+  image <- readPNG(files[i])
+  if(solid){
+    image_new <- image
+  } else{
+    image_gray <- apply(image, MARGIN = c(1,2), FUN = rgb2gray, method = "white")
+    #transpose array to be same as original
+    image_new <- aperm(image_gray, perm = c(2,3,1))
+  }
+  if(i %in% files_radiant){
+    image_new <- scale_alpha(image_new, alpha_scale_base)
+  }
+  #write out png
+  writePNG(image = image_new, target = paste0(files_no_ext[i], "_base.png"))
+}
+#tintable color
+for(i in seq_along(files)){
+  image <- readPNG(files[i])
+  if(solid){
+    image_new <- rgb2solid(image, color = "#FFFFFF")
+  } else{
+    image_gray <- apply(image, MARGIN = c(1,2), FUN = rgb2gray, method = "white")
+    #transpose array to be same as original
+    image_new <- aperm(image_gray, perm = c(2,3,1))
+  }
+  if(i %in% files_radiant){
+    image_new <- scale_alpha(image_new, alpha_scale_tint)
+  }
+  #write out png
+  writePNG(image = image_new, target = paste0(files_no_ext[i], "_tint.png"))
+}
+#other colors
 for(k in seq_along(solid_color)){
   for(i in seq_along(files)){
     image <- readPNG(files[i])
@@ -71,7 +127,10 @@ for(k in seq_along(solid_color)){
       #transpose array to be same as original
       image_new <- aperm(image_gray, perm = c(2,3,1))
     }
-    #write out grayscale png
+    if(i %in% files_radiant){
+      image_new <- scale_alpha(image_new, alpha_scale[k])
+    }
+    #write out png
     writePNG(image = image_new, target = paste0(files_no_ext[i], file_name_modifier[k], ".png"))
   }
 }
